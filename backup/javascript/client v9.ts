ceb0de6b782e2febecf8e2304f40b262cbe69b99
Alan <alan88w@yahoo.com>
2023-02-12 10:42:00 +0800

    added multiple examples

---
+++ backup/javascript/client v9.ts
@@ -0,0 +1,73 @@
+import * as THREE from 'three';
+import {
+  OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls';
+
+import Stats from 'three/examples/jsm/libs/stats.module';
+import {
+  GUI
+} from 'dat.gui';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.set(1.6, 1.7, 2)
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const material = new THREE.MeshNormalMaterial()
+let geometry = new THREE.BufferGeometry()
+const points = [
+  new THREE.Vector3(-1, 1, -1), //c
+  new THREE.Vector3(-1, -1, 1), //b
+  new THREE.Vector3(1, 1, 1), //a   
+
+  new THREE.Vector3(1, 1, 1), //a    
+  new THREE.Vector3(1, -1, -1), //d  
+  new THREE.Vector3(-1, 1, -1), //c
+
+  new THREE.Vector3(-1, -1, 1), //b
+  new THREE.Vector3(1, -1, -1), //d  
+  new THREE.Vector3(1, 1, 1), //a
+
+  new THREE.Vector3(-1, 1, -1), //c
+  new THREE.Vector3(1, -1, -1), //d    
+  new THREE.Vector3(-1, -1, 1), //b
+]
+
+geometry.setFromPoints(points)
+geometry.computeVertexNormals()
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh)
+
+const stats = Stats();
+document.body.appendChild(stats.dom);
+
+let data = {
+  x: 1
+}
+const gui = new GUI();
+
+gui.add(data, "x", -5, -1, 0.01).onChange(() => {
+  geometry.attributes.position.array[3] = data.x
+  geometry.attributes.position.needsUpdate = true
+})
+gui.open();
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  render();
+  stats.update();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+animate();
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,10 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -9,23 +12,42 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 2
+camera.position.z = 3
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', render) //this line is unnecessary if you are re-rendering within the animation loop
+new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+const boxGeometry = new THREE.BoxGeometry()
+const sphereGeometry = new THREE.SphereGeometry()
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
+const planeGeometry = new THREE.PlaneGeometry()
+const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const cube = new THREE.Mesh(geometry, material)
+const material = new THREE.MeshBasicMaterial()
+//const material= new THREE.MeshNormalMaterial()
+
+const cube = new THREE.Mesh(boxGeometry, material)
+cube.position.x = 5
 scene.add(cube)
+
+const sphere = new THREE.Mesh(sphereGeometry, material)
+sphere.position.x = 3
+scene.add(sphere)
+
+const icosahedron = new THREE.Mesh(icosahedronGeometry, material)
+icosahedron.position.x = 0
+scene.add(icosahedron)
+
+const plane = new THREE.Mesh(planeGeometry, material)
+plane.position.x = -2
+scene.add(plane)
+
+const torusKnot = new THREE.Mesh(torusKnotGeometry, material)
+torusKnot.position.x = -5
+scene.add(torusKnot)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -35,18 +57,49 @@ function onWindowResize() {
     render()
 }
 
-// function animate() {
-//     requestAnimationFrame(animate)
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
-//     cube.rotation.x += 0.01
-//     cube.rotation.y += 0.01
+const options = {
+    side: {
+        FrontSide: THREE.FrontSide,
+        BackSide: THREE.BackSide,
+        DoubleSide: THREE.DoubleSide,
+    },
+}
 
-//     render()
-// }
+const gui = new GUI()
+const materialFolder = gui.addFolder('THREE.Material')
+materialFolder
+    .add(material, 'transparent')
+    .onChange(() => (material.needsUpdate = true))
+materialFolder.add(material, 'opacity', 0, 1, 0.01)
+materialFolder.add(material, 'depthTest')
+materialFolder.add(material, 'depthWrite')
+materialFolder
+    .add(material, 'alphaTest', 0, 1, 0.01)
+    .onChange(() => updateMaterial())
+materialFolder.add(material, 'visible')
+materialFolder
+    .add(material, 'side', options.side)
+    .onChange(() => updateMaterial())
+materialFolder.open()
+
+function updateMaterial() {
+    material.side = Number(material.side)
+    material.needsUpdate = true
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    render()
+
+    stats.update()
+}
 
 function render() {
     renderer.render(scene, camera)
 }
 
-//animate()
-render()
+animate()
